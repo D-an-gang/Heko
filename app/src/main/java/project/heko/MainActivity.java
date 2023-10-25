d43616003a41d27e63b6f3f521c27e029efb8173
d1agnoze <dacvuong202@gmail.com>
2023-10-26 01:07:53 +0700

    to be done more

--- app/src/main/java/project/heko/MainActivity.java
+++ app/src/main/java/project/heko/MainActivity.java
@@ -1,12 +1,19 @@
 package project.heko;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.lifecycle.Observer;
@@ -19,6 +26,7 @@ import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+import project.heko.auth.LoginActivity;
 import project.heko.databinding.ActivityMainBinding;
 import project.heko.models.User;
 import project.heko.ui.navHeader.navHeaderViewModel;
@@ -50,6 +58,27 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+        //set up login activity result
+        ActivityResultLauncher<Intent> loginLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult o) {
+                int resCode = o.getResultCode();
+                Intent data = o.getData();
+
+                if (resCode == RESULT_OK) {
+                    Bundle extras = data.getExtras();
+                    if (extras != null || extras.getString("id") != null) {
+                        User user = new User(extras.getString("username"), extras.getString("email"), extras.getString("img"), extras.getString("id"));
+                        model.getUser().setValue(user);
+                    } else {
+                        Toast.makeText(MainActivity.this, getResources().getString(R.string.login_failed), Toast.LENGTH_SHORT).show();
+                        model.getUser().setValue(new User());
+                    }
+                }
+            }
+        });
+        //initialize login on click listener
+        authMethodInit(navigationView, loginLauncher);
     }
 
     @Override
@@ -66,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
                 || super.onSupportNavigateUp();
     }
 
-    private void liveDataInit(NavigationView navView) {
+    private void liveDataInit(@NonNull NavigationView navView) {
         model = new ViewModelProvider(this).get(navHeaderViewModel.class);
         View nav = navView.getHeaderView(0);
         TextView username = nav.findViewById(R.id.username);
@@ -90,5 +119,15 @@ public class MainActivity extends AppCompatActivity {
         model.getUser().observe(this, nameObserver);
     }
 
-
+    private void authMethodInit(@NonNull NavigationView navView, ActivityResultLauncher<Intent> loginLauncher) {
+        View nav = navView.getHeaderView(0);
+        nav.setOnClickListener(e -> {
+            if (model.getUser().getValue().getId() == null) {
+                Intent intent = new Intent(this, LoginActivity.class);
+                loginLauncher.launch(intent);
+            } else {
+                //TODO user profile page
+            }
+        });
+    }
 }
--- app/src/main/java/project/heko/auth/LoginActivity.java
+++ app/src/main/java/project/heko/auth/LoginActivity.java
@@ -1,13 +1,20 @@
 package project.heko.auth;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.SpannableString;
 import android.text.style.UnderlineSpan;
 
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import project.heko.R;
 import project.heko.databinding.ActivityLoginBinding;
+import project.heko.models.User;
 
 public class LoginActivity extends AppCompatActivity {
     private ActivityLoginBinding binding;
@@ -17,9 +24,54 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         setContentView(binding.getRoot());
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        //In gach chan nut sign up
         String mystring= getResources().getString(R.string.login_signup_link);
         SpannableString content = new SpannableString(mystring);
         content.setSpan(new UnderlineSpan(), 0, mystring.length(), 0);
         binding.signupLink.setText(content);
+        //back pressed -- Khi nguoi dung cancel
+        getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                cancelActivity();
+            }
+        });
+        //submit--khi nguoi dung bam dang nhap
+        binding.btnSubmit.setOnClickListener(e->{
+            //TODO dang nhap
+        });
+    }
+
+    public static boolean validateEmailAndPassword(String email, String password) {
+        boolean isEmailValid = validateEmail(email);
+        boolean isPasswordValid = validatePassword(password);
+
+        return isEmailValid && isPasswordValid;
+    }
+
+    private static boolean validateEmail(String email) {
+        // Email regex pattern for basic validation
+        String emailRegex = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
+        Pattern pattern = Pattern.compile(emailRegex);
+        Matcher matcher = pattern.matcher(email);
+
+        return matcher.matches();
+    }
+
+    private static boolean validatePassword(String password) {
+        return password.length() > 6;
+    }
+
+    private void cancelActivity(){
+        Intent intent = new Intent();
+        User user= new User();
+        Bundle bundle = new Bundle();
+        bundle.putString("username", user.getUsername());
+        bundle.putString("img", user.getImgUrl());
+        bundle.putString("email", user.getEmail());
+        bundle.putString("id", user.getId());
+        intent.putExtras(bundle);
+        setResult(Activity.RESULT_CANCELED, intent);
+        finish();
     }
 }
